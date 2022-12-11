59221266edf54f30d62d2e16dd0393dbcbacdc42
tarik-git <tarik6b@hotmail.com>
2022-12-11 18:53:10 +0100

    feat: Add Predavanje 7 initial project structure

---
+++ Predavanje7/app/src/main/java/org/tarik/numberfacts/MainActivity.java
@@ -0,0 +1,18 @@
+package org.tarik.numberfacts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//        implementation 'com.squareup.retrofit2:retrofit:2.9.0'
+//        implementation 'com.google.code.gson:gson:2.9.0'
+//        implementation 'com.squareup.retrofit2:converter-gson:2.9.0'
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
